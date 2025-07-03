7136b2c2f03ac18985b83d2b602cdaf89acea590
olegi80 <varme84@gmail.com>
2025-07-03 16:46:30 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+console.log('welcome to programming world')
+console.log(5+5)
+console.log(5-5)
+console.log(100/20)
+console.log(138*207)
+console.log(100+100/25)
+console.log(300-150)
+let mishaAge = 29
+console.log(mishaAge)
+console.log(100+400)
+console.log(2+3)
+console.log(200/100)
+console.log(820+1200)
+let mishasAge = 29
+console.log(mishasAge + 200)
+let n = 5
+n *= 3 
+console.log(n)
+let t = 5
+t++
+console.log(t)
+let f = 5
+f--
+console.log(f)
+
+console.log(true)
+console.log(false)
+let  loading = true 
+loading = false
+console.log(loading)
+console.log(15<10)
+console.log(4<10)
+
+
+console.log(2+2)
+console.log(100*100)
+let computer = 10 > 10
+console.log(computer)
+console.log(10==10)
+console.log(1!=10)
+console.log(10!=10)
+console.log(10<=10)
+console.log(9>=10)
+const barrier = 80
+let mishasResult = 50
+const testResult = mishasResult >= barrier
+console.log(testResult)
+const barriers = 80
+let mathResults = 20
+let englResults = 40
+let scResults = 0
+const testResults =mathResults + englResults + scResults >= barriers
+console.log(testResult) 
+let userLoggedIn = true
+let userPaid = true 
+const showProducts = userLoggedIn && userPaid 
+console.log(showProducts)
+let usersLoggedIn = false 
+let usersPaid = false 
+let usersIsAdmin = true 
+const Showproduct = usersIsAdmin || usersPaid 
+console.log(Showproduct)
+let useriLoogedIn = true
+let useriPaid = true 
+let useriAdmin = false 
+const showProducti = useriLoogedIn && (useriAdmin || useriPaid) 
+console.log(showProducti)
+const showLoginScreen = !useriLoogedIn 
+console.log(showLoginScreen)
+const showBuyButton = !(useriAdmin || useriPaid)
+console.log(showBuyButton) 
+const userIsaAdmin = true
+const discount = userIsaAdmin ? 25 : 0 
+console.log(discount) 
+console.log((userIsaAdmin ? 25 : 0) + 10 )
+const userDistance = 45
+const shippingCost = userDistance <= 50 ? 10 : 15 
+console.log(shippingCost)
+const useriIsAdmin =true
+if (useriIsAdmin) { console.log ("you are admin") }
+console.log("the end")
+console.log("githab")
